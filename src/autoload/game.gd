61213b81964f909f08abcc701f32cb3c59e50dc3
DanWaywell <34028193+DanWaywell@users.noreply.github.com>
2023-11-28 15:13:19 +0000

    Add simple level changer

--- src/autoload/game.gd
+++ src/autoload/game.gd
@@ -1,5 +1,11 @@
 extends Node
 
+const level_adderess_begining = "res://level/level_"
+const level_adderess_end = ".tscn"
+const first_level_adderess = "res://level/level_1000.tscn"
+
+var level_number = 1000
+
 
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
@@ -12,3 +18,16 @@ func _process(delta):
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 			DisplayServer.window_set_size(Vector2i(1280, 720))
 			DisplayServer.window_set_position(Vector2i(320, 180))
+
+
+func go_to_next_level():
+	# build level address
+	var next_level_number = level_number + 1
+	var next_level_adderess = level_adderess_begining + str(next_level_number) + level_adderess_end
+	# check if level exists and change to it or go back to level 0
+	if ResourceLoader.exists(next_level_adderess):
+		get_tree().change_scene_to_file(next_level_adderess)
+		level_number = int(next_level_adderess)
+	else:
+		get_tree().change_scene_to_file(first_level_adderess)
+		level_number = 1000
