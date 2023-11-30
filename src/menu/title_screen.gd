dd33ca9c32f1459502460e9a1a71b5c9970d6416
DanWaywell <34028193+DanWaywell@users.noreply.github.com>
2023-11-30 00:07:49 +0000

    Add check for controller input to start game

--- src/menu/title_screen.gd
+++ src/menu/title_screen.gd
@@ -3,6 +3,12 @@ extends Control
 
 func _ready():
 	$TextureButton.grab_focus()
+	
+
+func _process(delta):
+	if Input.is_action_just_pressed("jump"):
+		_on_texture_button_pressed()
+
 
 func _on_texture_button_pressed():
 	get_tree().change_scene_to_file("res://level/level_1000.tscn")
